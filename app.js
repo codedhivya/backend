cc34fe200eb8492636d624943bc03327cc748d73
Dhivya <divi27.1986td@gmail.com>
2023-12-21 13:59:44 +0530

    app changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
     res.json({
-        msg: "hello iam backend"
+        msg: "dhivya sending message to backend"
     })
 })
 app.listen(3000);
